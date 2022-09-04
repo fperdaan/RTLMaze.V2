4a2943f927a03a867f4d1c62076e189f3a9014e9
Ferdy Perdaan <f.perdaan@gmail.com>
2022-09-04 16:25:30 +0200

    Extend test setup

--- tests/RTLMaze.Core.Tests/JsonStreamConverter/Process.cs
+++ tests/RTLMaze.Core.Tests/JsonStreamConverter/Process.cs
@@ -1,3 +1,6 @@
+using System.Text;
+using System.Text.Json;
+
 namespace RTLMaze.Core.Tests.JsonStreamConverter;
 
 [TestClass]
@@ -7,29 +10,52 @@ public class Process_Should
     public void Convert_OnValidJsonInput()
     {
 		// Arrange 
+		var expectedResult = new JsonMockObject{ ID = 10, Name = "Test" };
+
+		var json = JsonSerializer.Serialize( expectedResult );
+		var stream = new MemoryStream( Encoding.UTF8.GetBytes( json ) );
+
+		var actor = new SUT.Models.JsonStreamConverter<JsonMockObject>();
 
 		// Act 
+		var result = actor.Process( stream );
 
 		// Assert
+		Assert.IsNotNull( result );
+		Assert.AreEqual( expected: expectedResult.GetType().Name, actual: result.GetType().Name );
+		Assert.AreEqual( expected: expectedResult?.ID, actual: result.ID );
+		Assert.AreEqual( expected: expectedResult?.Name, actual: result.Name );
     }
 
 	[TestMethod]
     public void ThrowException_OnInValidJsonInput()
     {
 		// Arrange 
+		var json = "Hello world!";
+		var stream = new MemoryStream( Encoding.UTF8.GetBytes( json ) );
 
-		// Act 
+		var actor = new SUT.Models.JsonStreamConverter<JsonMockObject>();
 
-		// Assert
+		// Act & Assert
+		Assert.ThrowsException<SUT.Exceptions.JsonParseException>( () => actor.Process( stream ) );
     }
 
 	[TestMethod]
     public void ThrowException_OnInUnexpectedJsonInput()
     {
 		// Arrange 
+		var json = JsonSerializer.Serialize( new int[]{1,2,3});
+		var stream = new MemoryStream( Encoding.UTF8.GetBytes( json ) );
 
-		// Act 
+		var actor = new SUT.Models.JsonStreamConverter<JsonMockObject>();
 
-		// Assert
+		// Act & Assert
+		Assert.ThrowsException<SUT.Exceptions.JsonParseException>( () => actor.Process( stream ) );
     }
+}
+
+internal class JsonMockObject 
+{
+	public int ID { get; set; }
+	public string? Name { get; set; }
 }
