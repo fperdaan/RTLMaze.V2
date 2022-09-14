9cf8f7d4c727481d75f32faf869488331becf372
Ferdy Perdaan <f.perdaan@gmail.com>
2022-09-14 14:32:59 +0200

    Add cast field to title

--- src/RTLMaze.Models/Title.cs
+++ src/RTLMaze.Models/Title.cs
@@ -6,7 +6,8 @@ public class Title
 	public int ID { get; set; }
 	public string Name { get; set; } = "";
 	public DateOnly? Premiered { get; set; }
-	
+	public virtual IEnumerable<Cast> Cast { get; set; } = new List<Cast>();
+
 	public override string ToString()
 	{
 		return $"{ID,8} / {Name} / {Premiered}";
